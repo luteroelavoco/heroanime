e22e7f94e8f22b3e5a309f40a462e37b52eff131
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-01-28 22:19:19 -0300

    feat: fake api for header background

---
+++ src/services/fakeapi.ts
@@ -0,0 +1,223 @@
+
+export const TopAnime = {
+  id: '13',
+  type: 'anime',
+  links: {
+    self: 'https://kitsu.io/api/edge/anime/13'
+  },
+  attributes: {
+    createdAt: '2013-02-20T16:00:26.866Z',
+    updatedAt: '2021-01-29T00:00:07.606Z',
+    slug: 'prince-of-tennis',
+    synopsis:
+      "The world of tennis is harsh and highly competitive. Numerous schools from Japan battle it out to determine the best of the best. Seishin Gakuen Junior High School, more commonly known as Seigaku, is one of the most prominent contestants in this battle of the finest. Their team line-up gets even stronger with the sudden arrival of a young prodigy from the West, Ryouma Echizen, who is determined to prove himself and escape the towering shadow of his legendary father. This fine addition changes the team forever.Prince of Tennis follows the heartwarming and inspirational story of Ryouma on his quest to become one of the best tennis players the country has ever seen. He pushes himself hard so that he can one day surpass his father’s name and his own personal expectations. Alongside the rest of the Seigaku team, Ryouma fights to make his and his teammate's dreams come true.",
+    description:
+      "The world of tennis is harsh and highly competitive. Numerous schools from Japan battle it out to determine the best of the best. Seishin Gakuen Junior High School, more commonly known as Seigaku, is one of the most prominent contestants in this battle of the finest. Their team line-up gets even stronger with the sudden arrival of a young prodigy from the West, Ryouma Echizen, who is determined to prove himself and escape the towering shadow of his legendary father. This fine addition changes the team forever.Prince of Tennis follows the heartwarming and inspirational story of Ryouma on his quest to become one of the best tennis players the country has ever seen. He pushes himself hard so that he can one day surpass his father’s name and his own personal expectations. Alongside the rest of the Seigaku team, Ryouma fights to make his and his teammate's dreams come true.",
+    coverImageTopOffset: 200,
+    titles: {
+      en: 'The Prince of Tennis',
+      en_jp: 'Prince of Tennis',
+      ja_jp: 'テニスの王子様'
+    },
+    canonicalTitle: 'Prince of Tennis',
+    abbreviatedTitles: ['Tennis no Ouji-sama'],
+    averageRating: '77.96',
+    ratingFrequencies: {
+      2: '42',
+      3: '0',
+      4: '25',
+      5: '2',
+      6: '30',
+      7: '2',
+      8: '121',
+      9: '0',
+      10: '212',
+      11: '2',
+      12: '513',
+      13: '13',
+      14: '828',
+      15: '20',
+      16: '1049',
+      17: '15',
+      18: '519',
+      19: '8',
+      20: '1066'
+    },
+    userCount: 10306,
+    favoritesCount: 147,
+    startDate: '2001-10-10',
+    endDate: '2005-03-23',
+    nextRelease: null,
+    popularityRank: 1146,
+    ratingRank: 962,
+    ageRating: 'PG',
+    ageRatingGuide: 'Teens 13 or older',
+    subtype: 'TV',
+    status: 'finished',
+    tba: '',
+    posterImage: {
+      tiny: 'https://media.kitsu.io/anime/poster_images/13/tiny.jpg?1597604267',
+      small:
+        'https://media.kitsu.io/anime/poster_images/13/small.jpg?1597604267',
+      medium:
+        'https://media.kitsu.io/anime/poster_images/13/medium.jpg?1597604267',
+      large:
+        'https://media.kitsu.io/anime/poster_images/13/large.jpg?1597604267',
+      original:
+        'https://media.kitsu.io/anime/poster_images/13/original.jpg?1597604267',
+      meta: {
+        dimensions: {
+          tiny: {
+            width: 110,
+            height: 156
+          },
+          small: {
+            width: 284,
+            height: 402
+          },
+          medium: {
+            width: 390,
+            height: 554
+          },
+          large: {
+            width: 550,
+            height: 780
+          }
+        }
+      }
+    },
+    coverImage: {
+      tiny: 'https://media.kitsu.io/anime/cover_images/13/tiny.jpg?1597604272',
+      small:
+        'https://media.kitsu.io/anime/cover_images/13/small.jpg?1597604272',
+      large:
+        'https://media.kitsu.io/anime/cover_images/13/large.jpg?1597604272',
+      original:
+        'https://media.kitsu.io/anime/cover_images/13/original.png?1597604272',
+      meta: {
+        dimensions: {
+          tiny: {
+            width: 840,
+            height: 200
+          },
+          small: {
+            width: 1680,
+            height: 400
+          },
+          large: {
+            width: 3360,
+            height: 800
+          }
+        }
+      }
+    },
+    episodeCount: 178,
+    episodeLength: 22,
+    totalLength: 3916,
+    youtubeVideoId: '',
+    showType: 'TV',
+    nsfw: false
+  },
+  relationships: {
+    genres: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/genres',
+        related: 'https://kitsu.io/api/edge/anime/13/genres'
+      }
+    },
+    categories: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/categories',
+        related: 'https://kitsu.io/api/edge/anime/13/categories'
+      }
+    },
+    castings: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/castings',
+        related: 'https://kitsu.io/api/edge/anime/13/castings'
+      }
+    },
+    installments: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/installments',
+        related: 'https://kitsu.io/api/edge/anime/13/installments'
+      }
+    },
+    mappings: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/mappings',
+        related: 'https://kitsu.io/api/edge/anime/13/mappings'
+      }
+    },
+    reviews: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/reviews',
+        related: 'https://kitsu.io/api/edge/anime/13/reviews'
+      }
+    },
+    mediaRelationships: {
+      links: {
+        self:
+          'https://kitsu.io/api/edge/anime/13/relationships/media-relationships',
+        related: 'https://kitsu.io/api/edge/anime/13/media-relationships'
+      }
+    },
+    characters: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/characters',
+        related: 'https://kitsu.io/api/edge/anime/13/characters'
+      }
+    },
+    staff: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/staff',
+        related: 'https://kitsu.io/api/edge/anime/13/staff'
+      }
+    },
+    productions: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/productions',
+        related: 'https://kitsu.io/api/edge/anime/13/productions'
+      }
+    },
+    quotes: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/quotes',
+        related: 'https://kitsu.io/api/edge/anime/13/quotes'
+      }
+    },
+    episodes: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/episodes',
+        related: 'https://kitsu.io/api/edge/anime/13/episodes'
+      }
+    },
+    streamingLinks: {
+      links: {
+        self:
+          'https://kitsu.io/api/edge/anime/13/relationships/streaming-links',
+        related: 'https://kitsu.io/api/edge/anime/13/streaming-links'
+      }
+    },
+    animeProductions: {
+      links: {
+        self:
+          'https://kitsu.io/api/edge/anime/13/relationships/anime-productions',
+        related: 'https://kitsu.io/api/edge/anime/13/anime-productions'
+      }
+    },
+    animeCharacters: {
+      links: {
+        self:
+          'https://kitsu.io/api/edge/anime/13/relationships/anime-characters',
+        related: 'https://kitsu.io/api/edge/anime/13/anime-characters'
+      }
+    },
+    animeStaff: {
+      links: {
+        self: 'https://kitsu.io/api/edge/anime/13/relationships/anime-staff',
+        related: 'https://kitsu.io/api/edge/anime/13/anime-staff'
+      }
+    }
+  }
+}
