89c695e731ce08574809d8de3061e2f9a624291b
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-02 11:57:46 -0300

    feat: global theme.less

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const path = require('path')
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+  fs.readFileSync(path.resolve(__dirname, './src/styles/theme.less'), 'utf8')
 )
 
 module.exports = withLess({
