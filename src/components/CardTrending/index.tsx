dcd7796b3d495bd529859faebfd01b69273544ef
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-02 21:03:03 -0300

    feat: card trending

--- src/components/CardTrending/index.tsx
+++ src/components/CardTrending/index.tsx
@@ -1,42 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles.less'
 import { useRouter } from 'next/router'
-import { Anime } from '../../services/animeType'
+import { Card, Button } from 'antd'
 import { Abbreviate } from '../../utils/anime'
-import {
-  Container,
-  Image,
-  Content,
-  Title,
-  SubTitle,
-  Desc,
-  SeeMore
-} from './styles'
+import { animeProps } from '../../helpers/interfaces'
 
-interface props {
-  anime: Anime
-}
-const CardTrending: React.FC<props> = ({ anime }) => {
+const { Meta } = Card
+
+const CardTrending: React.FC<animeProps> = ({ anime }) => {
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
 
   function handleNextPage() {
+    setLoading(true)
     router.push(`/anime/${anime.id}`)
   }
   return (
-    <Container>
-      <Image src={anime.attributes.posterImage.original} alt={anime.attributes.canonicalTitle} />
-      <Content>
-        <Title onClick={() => handleNextPage()}>
-          {' '}
-          {anime.attributes.canonicalTitle}{' '}
-        </Title>
-        <SubTitle onClick={() => handleNextPage()}>
-          {' '}
-          Available on {anime.attributes.showType}
-        </SubTitle>
-        <Desc> Synopsis : {Abbreviate(anime.attributes.synopsis,15)}</Desc>
-        <SeeMore  onClick={() => handleNextPage()}> See more </SeeMore>
-      </Content>
-    </Container>
+    <div className="container-card-trending">
+      <Card
+
+        hoverable
+        style={{ width: '100%' }}
+        onClick={() => handleNextPage()}
+        cover={
+          <img
+            alt={anime.attributes.canonicalTitle}
+            src={anime.attributes.posterImage.original}
+          />
+        }
+      >
+        <Meta
+          title={anime.attributes.canonicalTitle}
+          description={Abbreviate(anime.attributes.synopsis, 15)}
+        />
+        <Button type="primary" loading={loading} onClick={handleNextPage}> See more</Button>
+      </Card>
+    </div>
   )
 }
 
